b350984594831e771a34939f5ea3825001f95a12
rodriguesfe <nandda.rodrigues@gmail.com>
2017-10-25 11:37:59 -0200

    Update setup.py

--- BookTruck/setup.py
+++ BookTruck/setup.py
@@ -1,6 +1,25 @@
-from App import db, models
+from BookApp import db, models
 from copy import copy
 
 db.drop_all()
 db.create_all()
+
+
+'''Cria usuários administradores'''
+nome = 'admin'
+user = 'admin'
+senha = 1234
+
+admin = models.Usuario(user=user, nome=nome, senha=senha, tipo=1)
+db.session.add(admin)
+db.session.commit()
+
+nome = 'adm'
+user = 'adm'
+senha = 5678
+
+admin = models.Usuario(user=user, nome=nome, senha=senha, tipo=1)
+db.session.add(admin)
+db.session.commit()
+
 print('done')
